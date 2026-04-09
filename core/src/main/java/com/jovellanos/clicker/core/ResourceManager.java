f607cb0854cb45faf8bcd3f29370f639ade033fe
marcos.jimenez <marcos.jimenez@mercanza.net>
2026-04-09 13:09:59 +0200

    Arreglar Issue Medium en ResourceManager

--- core/src/main/java/com/jovellanos/clicker/core/ResourceManager.java
+++ core/src/main/java/com/jovellanos/clicker/core/ResourceManager.java
@@ -6,6 +6,11 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 public class ResourceManager {
     private static Skin skin;
 
+    //Necesario para asegurarse de que no se haga un new resourceManager
+    private ResourceManager() {
+        throw new IllegalStateException("Clase de utilidad - No instanciar"); 
+    }
+
     public static void load() {
         // Carga el archivo generado con SkinComposer
         skin = new Skin(Gdx.files.internal("skin/Skin.json"));
